83d102fb4eef70fe7fea63b333e565ea7542cb89
Nastya Arisova <arisova.anastasia25@gmail.com>
2021-12-01 23:02:52 +0300

    add a view with current time

--- app/src/main/java/com/example/retrorally/JudgeFragment.kt
+++ app/src/main/java/com/example/retrorally/JudgeFragment.kt
@@ -1,24 +1,31 @@
 package com.example.retrorally
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.retrorally.databinding.FragmentJudgeBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class JudgeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var binding: FragmentJudgeBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_judge, container, false)
+        binding = FragmentJudgeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
 }
