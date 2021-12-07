99fd7c0d16e5185bea4ca38dc163030c030c111f
Nastya Arisova <arisova.anastasia25@gmail.com>
2021-12-07 23:02:15 +0300

    set a working buttons

--- app/src/main/java/com/example/retrorally/JudgeFragment.kt
+++ app/src/main/java/com/example/retrorally/JudgeFragment.kt
@@ -4,28 +4,58 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.retrorally.databinding.FragmentJudgeBinding
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class JudgeFragment : Fragment() {
 
-    private var binding: FragmentJudgeBinding? = null
+    private var mainBinding: FragmentJudgeBinding? = null
+    private val one = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentJudgeBinding.inflate(inflater, container, false)
-        return binding?.root
+        mainBinding = FragmentJudgeBinding.inflate(inflater, container, false)
+        return mainBinding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupCarNumberInput()
+    }
 
+    private fun setupCarNumberInput() {
+        mainBinding?.keyboard?.apply {
+            one.setupNumberInputButton(1)
+            two.setupNumberInputButton(2)
+            three.setupNumberInputButton(3)
+            four.setupNumberInputButton(4)
+            five.setupNumberInputButton(5)
+            six.setupNumberInputButton(6)
+            seven.setupNumberInputButton(7)
+            eight.setupNumberInputButton(8)
+            nine.setupNumberInputButton(9)
+            nul.setupNumberInputButton(0)
+            cancel.setOnClickListener {
+                mainBinding?.enterCar?.number?.apply {
+                    if (text.isNotEmpty()) {
+                        text = text.toString()
+                            .substring(startIndex = 0, endIndex = text.toString().length - 1)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun Button.setupNumberInputButton(num: Int) {
+        setOnClickListener {
+            mainBinding?.enterCar?.number?.apply {
+                text = text.toString() + num
+            }
+        }
     }
 
 }
